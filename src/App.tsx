8b24dab777c0f8e7cdacdba31be6c3f7901d0088
Eryk Olliver <erykolliver@gmail.com>
2025-11-11 16:36:27 -0300

    feat: consumo da API de login efetuada

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,50 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [email, setMail] = useState("")
+  const [password, setPass] = useState("")
+  const [errorMsg, setErro] = useState("")
+
+  const sendForm = async (e: any) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      setErro("Email ou senha incorretos")
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:3000/login", {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password })
+      });
+
+      if (!response.ok) {
+        throw new Error(`Erro: ${response.status}: falha no login`)
+      }
+
+      const data = await response.json()
+
+      console.log("Enviado")
+
+      if (response.ok && data.success) {
+        console.log("✅ Login Efetuado com Sucesso!", data)
+        setErro(`✅ Login Efetuado com Sucesso! ${data.user.email}`),
+          alert(`seja Bem vindo: ${data.token}`)
+      } else {
+        setErro(data.message || 'Erro ao fazer login')
+      }
+
+    } catch (err: any) {
+      console.log(`Erro detectado!: ${err}`)
+      setErro('Erro ao conectar ao servidor!  ')
+    }
+
+  }
 
   return (
     <section className='w-screen h-screen flex-col gap-5 justify-center flex items-center'>
@@ -13,17 +57,20 @@ function App() {
         </div> */}
         <div className='flex w-full gap-1.5 flex-col items-start justify-between'>
           <label htmlFor="" className='text-2xl w-full'> E-mail</label>
-          <input type="email" name="" id="" className='w-full text-lg focus:outline-none bg-gray-200/70 p-2.5 text-black/50' />
+          <input type="email" value={email} onChange={(e) => setMail(e.target.value)} placeholder='example@gmail.com' name="" id="" className='w-full text-lg focus:outline-none bg-gray-200/70 p-2.5 text-black/50' />
         </div>
         <div className='flex w-full gap-1.5 flex-col items-start justify-between'>
           <label htmlFor="" className='text-2xl w-full'> Senha</label>
-          <input type="text" name="" id="" className='w-full text-lg focus:outline-none bg-gray-200/70 p-2.5 text-black/50' />
+          <input type="text" value={password} onChange={(e) => setPass(e.target.value)} placeholder='Ex. 12345678910' name="" id="" className='w-full text-lg focus:outline-none bg-gray-200/70 p-2.5 text-black/50' />
         </div>
         <div className='w-full flex flex-col gap-1.5'>
           <hr className='w-full' />
-          <button className='p-5 hover:cursor-pointer border w-full bg-orange-400 text-white text-2xl font-bold'>Criar Conta</button>
+          <button onClick={sendForm} className='p-5 hover:cursor-pointer border w-full bg-orange-400 text-white text-2xl font-bold'>Criar Conta</button>
         </div>
       </form>
+      {errorMsg && (
+        <div>{errorMsg}</div>
+      )}
     </section>
   )
 }
